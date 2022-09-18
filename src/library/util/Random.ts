c09d36255907d8af5b325ae1043d87d84459003e
Oliver Böhm <oliverboehmprivat@gmail.com>
2022-09-18 23:02:02 +0200

    docs.(random): add documentation

--- src/library/util/Random.ts
+++ src/library/util/Random.ts
@@ -1,9 +1,20 @@
+/**
+ * Returns a random int between min (inclusive) and max (inclusive)
+ * @param min
+ * @param max
+ * @returns
+ */
 export function randomInt(min: number, max: number): number {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+/**
+ * Generates a random string of a given length
+ * @param length
+ * @returns
+ */
 export function randomString(length: number): string {
   let random = '';
   for (let i = 0; i < length; i++) {
