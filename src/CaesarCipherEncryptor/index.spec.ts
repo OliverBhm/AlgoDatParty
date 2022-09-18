749245caf8e8229d1061f5e5262388e0f3d0b560
Oliver Böhm <oliverboehmprivat@gmail.com>
2022-09-18 22:49:20 +0200

    Remove node modules

--- src/CaesarCipherEncryptor/index.spec.ts
+++ src/CaesarCipherEncryptor/index.spec.ts
@@ -1,10 +1,20 @@
-import { encrypt } from '.';
+import { decrypt, encrypt } from '.';
+import { randomInt, randomString } from '../library/util/Random';
 describe('CaesarCipherEncryptor', () => {
   it('should encrypt a string with given shift', () => {
     expect(encrypt('abc', 2)).toEqual('cde');
   });
 
-  it('should wrap on overfloew', () => {
+  it('should wrap on overflow', () => {
     expect(encrypt('xyz', 2)).toEqual('zab');
   });
+
+  /*it('', () => {
+    for (let i = 0; i < 1000; i++) {
+      const randomShift: number = randomInt(0, 1000);
+      const hay: string = randomString(10);
+      const encrypted: string = encrypt(hay, randomShift);
+      expect(decrypt(encrypted, randomShift)).toEqual(hay);
+    }
+  });*/
 });
