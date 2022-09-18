c8c87b3ac7488080d2908d131c8d04beb3634dcb
Oliver Böhm <oliverboehmprivat@gmail.com>
2022-09-18 15:56:17 +0200

    fix(type/comparator): Format

--- src/library/type/Comparator.ts
+++ src/library/type/Comparator.ts
@@ -1,12 +1,12 @@
 /**
- * A function determining the order of elements by comparing value `a` against `b`. 
+ * A function determining the order of elements by comparing value `a` against `b`.
  * Should return a negative number for less than, 0 for equal to and a positive number for greater than.
  */
- export type ComparisonFunction = <T>(a: T, b: T) => number
+export type ComparisonFunction = <T>(a: T, b: T) => number;
 
- /**
-  * Findes elements by a truth check. 
-  * Should return `true` for the element you're trying to match. 
-  * Otherwise return `false`  
-  */
-  export type Predicate = <T>(a: T) => boolean
+/**
+ * Findes elements by a truth check.
+ * Should return `true` for the element you're trying to match.
+ * Otherwise return `false`
+ */
+export type Predicate = <T>(a: T) => boolean;
