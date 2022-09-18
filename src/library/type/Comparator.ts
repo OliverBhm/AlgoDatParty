adaf9834142090bd72d8740cb781848908ebe32c
Oliver Böhm <oliverboehmprivat@gmail.com>
2022-09-18 15:01:08 +0200

    feat(type): Add Predicate

--- src/library/type/Comparator.ts
+++ src/library/type/Comparator.ts
@@ -3,3 +3,10 @@
  * Should return a negative number for less than, 0 for equal to and a positive number for greater than.
  */
  export type ComparisonFunction = <T>(a: T, b: T) => number
+
+ /**
+  * Findes elements by a truth check. 
+  * Should return `true` for the element you're trying to match. 
+  * Otherwise return `false`  
+  */
+  export type Predicate = <T>(a: T) => boolean
