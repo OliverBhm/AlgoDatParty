e84d5ecc219cfd7a1d939e8bea3b0c689c6c08f7
Oliver Böhm <oliverboehmprivat@gmail.com>
2022-12-14 15:01:34 +0100

    feat(BinarySearch): Add function to find element

--- src/BinarySearch/index.ts
+++ src/BinarySearch/index.ts
@@ -31,3 +31,24 @@
     
     return -start - 1;
   }
+
+  /**
+ * Finds an element in an array.
+ * @param haystack A sorted array
+ * @param comparisonFn Function used to determine how to travers the array to find the `target`. 
+ * It is expected to return a negative value if the element is less than the `target`, zero if 
+ * the element is matched, and a positive value otherwise. 
+ * @example 
+ * // Finding a number:
+ * const haystack: Array<number> = [10, 20, 30, 40, 50, 60]
+   binarySearch(haystack, ((a) => 50 - a)) // 50
+   @example 
+   // Finding an object by a property: 
+ * const haystack: { name: string } = [{name: 'Alhpa'}, {name: 'Beta'}, {name: 'Gamma'}, {name: 'Zeta'}]
+   binarySearch(haystack, (({ name }) => 'Gamma'.localeCompare(name))) // { name: Gamma}
+ * @returns The the element `undefined` if there is no match.
+ */
+   function binarySearchElement<T>(haystack: Array<T>, comparisonFn: (value: T) => number): T | undefined 
+   {
+      return haystack[binarySearch(haystack, comparisonFn)] || undefined
+   }
