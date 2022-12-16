143e6522601b4e46ea9df64c2d8e089ababb65ee
Oliver Böhm <oliverboehmprivat@gmail.com>
2022-12-16 11:14:25 +0100

    test(IsPalindrome): Add test cases for false check

--- src/IsPalindrome/index.spec.ts
+++ src/IsPalindrome/index.spec.ts
@@ -7,10 +7,15 @@ describe('isPalindrome', () => {
       const test: string = randomString(randomInt(1, 100));
       const reversed = [...test].reverse().join('');
       expect(isPalindrome(`${test}${reversed}`)).toBeTruthy();
+      expect(isPalindrome(`${test}`)).toBeFa();
     }
   });
   
   it('should check if a short string is a palindrome', () => {
       expect(isPalindrome('a')).toBeTruthy();
+  });
+  
+  it('should check if a string is not a palindrome', () => {
+      expect(isPalindrome('abc')).toBeFalsy();
   });
 });
